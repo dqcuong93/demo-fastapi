38fd9cda0d33dd3e49bee9377a471f7661981007
Chris D <dqcuong93@gmail.com>
2020-10-08 18:19:21 +0700

    Update comments

--- stock-screener/main.py
+++ stock-screener/main.py
@@ -5,19 +5,26 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Define you template language and its directory
 templates = Jinja2Templates(directory='templates')
 
 
 @app.get("/")
 def home(request: Request):
+    """
+    This is the root path - home path
+    :param request:
+    :return:
+    """
     return templates.TemplateResponse("home.html", {
         "request": request,
-        "test_var": 1993,
+        "test_var": 1993,  # Call this variable in your template using Jinja2 syntax
     })
 
 
 @app.post("/stock")
 def create_stock():
+    # Return a JSON response
     return {
         "code": "success",
         "message": "Stock created"
