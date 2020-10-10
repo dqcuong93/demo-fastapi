ddf8f6160cf823fc2d109f5376e102656d92191c
Chris D <dqcuong93@gmail.com>
2020-10-10 09:35:41 +0700

    Code format checked

--- stock-screener/main.py
+++ stock-screener/main.py
@@ -1,5 +1,5 @@
 import models
-from database import engine, SessionLocal
+from database import SessionLocal, engine
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
